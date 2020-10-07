75bdd34afe50f493dee4f716f89bdd471f487ceb
Steve Glaser <sglaser@nvidia.com>
2020-10-06 18:18:08 -0700

    Rename scripts directory to old-scripts

--- Spec/old-scripts/fixup-2016.js
+++ Spec/old-scripts/fixup-2016.js
@@ -1,11 +1,13 @@
 /******************************************************************************
  *                 JS Extension for the W3C Spec Style Sheet                  *
  *                                                                            *
- * This code handles some fixup to improve the table of contents.             *
- * It is intended to be a very simple script for 2016.                        *
+ * This code handles:                                                         *
+ * - some fixup to improve the table of contents                              *
+ * - the obsolete warning on outdated specs                                   *
  ******************************************************************************/
 (function() {
   "use strict";
+  var ESCAPEKEY = 27;
   var collapseSidebarText = '<span aria-hidden="true">←</span> '
                           + '<span>Collapse Sidebar</span>';
   var expandSidebarText   = '<span aria-hidden="true">→</span> '
@@ -114,8 +116,15 @@
        (window too small for the MQ to add the margin to body),
        then auto-close the sidebar once you click on something in there. */
     toc.addEventListener('click', function(e) {
-      if(e.target.tagName.toLowerCase() == "a" && document.body.classList.contains('toc-sidebar') && !sidebarMedia.matches) {
-        toggleSidebar(false);
+      if(document.body.classList.contains('toc-sidebar') && !sidebarMedia.matches) {
+        var el = e.target;
+        while (el != toc) { // find closest <a>
+          if (el.tagName.toLowerCase() == "a") {
+            toggleSidebar(false);
+            return;
+          }
+          el = el.parentElement;
+        }
       }
     }, false);
   }
@@ -139,4 +148,94 @@
     }
   }
 
+  /* Deprecation warning */
+  if (document.location.hostname === "www.w3.org" && /^\/TR\/\d{4}\//.test(document.location.pathname)) {
+    var request = new XMLHttpRequest();
+
+    request.open('GET', 'https://www.w3.org/TR/tr-outdated-spec');
+    request.onload = function() {
+      if (request.status < 200 || request.status >= 400) {
+        return;
+      }
+      try {
+        var currentSpec = JSON.parse(request.responseText);
+      } catch (err) {
+        console.error(err);
+        return;
+      }
+      document.body.classList.add("outdated-spec");
+      var node = document.createElement("p");
+      node.classList.add("outdated-warning");
+      node.tabIndex = -1;
+      node.setAttribute("role", "dialog");
+      node.setAttribute("aria-modal", "true");
+      node.setAttribute("aria-labelledby", "outdatedWarning");
+      if (currentSpec.style) {
+          node.classList.add(currentSpec.style);
+      }
+
+      var frag = document.createDocumentFragment();
+      var heading = document.createElement("strong");
+      heading.id = "outdatedWarning";
+      heading.innerHTML = currentSpec.header;
+      frag.appendChild(heading);
+
+      var anchor = document.createElement("a");
+      anchor.href = currentSpec.latestUrl;
+      anchor.innerText = currentSpec.latestUrl + ".";
+
+      var warning = document.createElement("span");
+      warning.innerText = currentSpec.warning;
+      warning.appendChild(anchor);
+      frag.appendChild(warning);
+
+      var button = document.createElement("button");
+      var handler = makeClickHandler(node);
+      button.addEventListener("click", handler);
+      button.innerHTML = "&#9662; collapse";
+      frag.appendChild(button);
+      node.appendChild(frag);
+
+      function makeClickHandler(node) {
+        var isOpen = true;
+        return function collapseWarning(event) {
+          var button = event.target;
+          isOpen = !isOpen;
+          node.classList.toggle("outdated-collapsed");
+          document.body.classList.toggle("outdated-spec");
+          button.innerText = (isOpen) ? '\u25BE collapse' : '\u25B4 expand';
+        }
+      }
+
+      document.body.appendChild(node);
+      button.focus();
+      window.onkeydown = function (event) {
+        var isCollapsed = node.classList.contains("outdated-collapsed");
+        if (event.keyCode === ESCAPEKEY && !isCollapsed) {
+          button.click();
+        }
+      }
+
+      window.addEventListener("click", function(event) {
+        if (!node.contains(event.target) && !node.classList.contains("outdated-collapsed")) {
+          button.click();
+        }
+      });
+
+      document.addEventListener("focus", function(event) {
+        var isCollapsed = node.classList.contains("outdated-collapsed");
+        var containsTarget = node.contains(event.target);
+        if (!isCollapsed && !containsTarget) {
+          event.stopPropagation();
+          node.focus();
+        }
+      }, true); // use capture to enable event delegation as focus doesn't bubble up
+    };
+
+    request.onerror = function() {
+      console.error("Request to https://www.w3.org/TR/tr-outdated-spec failed.");
+    };
+
+    request.send();
+  }
 })();
