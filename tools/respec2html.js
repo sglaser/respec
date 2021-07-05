3ddb7ba37756fb614079d40eb079a62b6676a717
Steve Glaser <sglaser@nvidia.com>
2021-07-04 19:45:38 -0700

    Update respec2html and respecDocWriter to latest

--- tools/respec2html.js
+++ tools/respec2html.js
@@ -1,7 +1,30 @@
 #!/usr/bin/env node
+const path = require("path");
+const http = require("http");
+const serveStatic = require("serve-static");
+const finalhandler = require("finalhandler");
 const sade = require("sade");
 const colors = require("colors");
-const { toHTML, write } = require("./respecDocWriter");
+const marked = require("marked");
+const { toHTML, write } = require("./respecDocWriter.js");
+
+class Renderer extends marked.Renderer {
+  strong(text) {
+    return colors.bold(text);
+  }
+  em(text) {
+    return colors.italic(text);
+  }
+  codespan(text) {
+    return colors.underline(text);
+  }
+  paragraph(text) {
+    return text;
+  }
+  link(href, _title, text) {
+    return `[${text}](${colors.blue.dim.underline(href)})`;
+  }
+}
 
 class Logger {
   /** @param {boolean} verbose */
@@ -15,26 +38,100 @@ class Logger {
    */
   info(message, timeRemaining) {
     if (!this.verbose) return;
-    console.log(`[Timeout: ${timeRemaining}ms] ${message}`);
+    const header = colors.dim.bgWhite.black.bold("[INFO]");
+    const time = colors.dim(`[Timeout: ${timeRemaining}ms]`);
+    console.error(header, time, message);
   }
 
-  /** @param {{ message: string }} rsError */
+  /**
+   * @typedef {import("./respecDocWriter.js").RsError} RsError
+   * @param {RsError} rsError
+   */
   error(rsError) {
-    console.error(
-      colors.red(`💥 ReSpec error: ${colors.cyan(rsError.message)}`)
-    );
+    const header = colors.bgRed.white.bold("[ERROR]");
+    const message = colors.red(this._formatMarkdown(rsError.message));
+    console.error(header, message);
+    if (rsError.plugin) {
+      this._printDetails(rsError);
+    }
   }
 
-  /** @param {{ message: string }} rsError */
+  /** @param {RsError} rsError */
   warn(rsError) {
-    console.warn(
-      colors.yellow(`⚠️ ReSpec warning: ${colors.cyan(rsError.message)}`)
-    );
+    const header = colors.bgYellow.black.bold("[WARNING]");
+    const message = colors.yellow(this._formatMarkdown(rsError.message));
+    console.error(header, message);
+    if (rsError.plugin) {
+      this._printDetails(rsError);
+    }
   }
 
   /** @param {Error | string} error */
   fatal(error) {
-    console.error(colors.red(error.stack || error));
+    const header = colors.bgRed.white.bold("[FATAL]");
+    const message = colors.red(error.stack || error);
+    console.error(header, message);
+  }
+
+  _formatMarkdown(str) {
+    if (typeof str !== "string") return str;
+    return marked(str, { smartypants: true, renderer: new Renderer() });
+  }
+
+  /** @param {import("./respecDocWriter").ReSpecError} rsError */
+  _printDetails(rsError) {
+    const print = (title, value) => {
+      if (!value) return;
+      const padWidth = "Plugin".length + 1; // "Plugin" is the longest title
+      const paddedTitle = `${title}:`.padStart(padWidth);
+      console.error(" ", colors.bold(paddedTitle), this._formatMarkdown(value));
+    };
+    print("Count", rsError.elements && String(rsError.elements.length));
+    print("Plugin", rsError.plugin);
+    print("Hint", rsError.hint);
+  }
+}
+
+class StaticServer {
+  /**
+   * @param {number} port
+   * @param {string} source
+   */
+  constructor(port, source) {
+    if (path.isAbsolute(source) || /^(\w+:\/\/)/.test(source.trim())) {
+      const msg = `Invalid path for use with --localhost. Only relative paths allowed.`;
+      const hint =
+        "Please ensure your ReSpec document and its local resources" +
+        " (e.g., data-includes) are accessible from the current working directory.";
+      throw new Error(`${msg} ${hint}`);
+    }
+
+    if (port && isNaN(parseInt(port, 10))) {
+      throw new Error("Invalid port number.");
+    }
+
+    this.port = port;
+    this.source = source;
+
+    const serve = serveStatic(process.cwd());
+    this.server = http.createServer((req, res) => {
+      serve(req, res, finalhandler(req, res));
+    });
+  }
+
+  get url() {
+    return new URL(this.source, `http://localhost:${this.port}/`);
+  }
+
+  async start() {
+    await new Promise((resolve, reject) => {
+      this.server.listen(this.port, resolve);
+      this.server.on("error", reject);
+    });
+  }
+
+  async stop() {
+    await new Promise(resolve => this.server.close(resolve));
   }
 }
 
@@ -49,7 +146,12 @@ const cli = sade("respec [source] [destination]", true)
       "# Halt on errors or warning."
     )}`
   )
-  .example("--src http://example.com/spec.html --out spec.html");
+  .example("--src http://example.com/spec.html --out spec.html")
+  .example(
+    `--localhost index.html out.html ${colors.dim(
+      "# Generate file using a local web server."
+    )}`
+  );
 
 cli
   // For backward compatibility
@@ -61,16 +163,23 @@ cli
     "How long to wait before timing out (in seconds).",
     10
   )
+  .option(
+    "--use-local",
+    "Use locally installed ReSpec instead of the one in document.",
+    false
+  )
   .option("-e, --haltonerror", "Abort if the spec has any errors.", false)
   .option("-w, --haltonwarn", "Abort if ReSpec generates warnings.", false)
   .option("--disable-sandbox", "Disable Chromium sandboxing if needed.", false)
   .option("--devtools", "Enable debugging and show Chrome's DevTools.", false)
-  .option("--verbose", "Log processing status to stdout.", false);
+  .option("--verbose", "Log processing status to stdout.", false)
+  .option("--localhost", "Spin up a local server to perform processing.", false)
+  .option("--port", "Port override for --localhost.", 3000);
 
 cli.action((source, destination, opts) => {
   source = source || opts.src;
   destination = destination || opts.out;
-  const log = new Logger(opts.verbose && destination !== "stdout");
+  const log = new Logger(opts.verbose);
 
   if (!source) {
     log.fatal("A source is required.");
@@ -90,7 +199,12 @@ cli._version = () => {
   console.log(version);
 };
 
-cli.parse(process.argv);
+cli.parse(process.argv, {
+  unknown(flag) {
+    new Logger().fatal(`Unknown option: ${flag}`);
+    process.exit(1);
+  },
+});
 
 /**
  * @param {string} source
@@ -99,10 +213,19 @@ cli.parse(process.argv);
  * @param {Logger} log
  */
 async function run(source, destination, options, log) {
-  const src = new URL(source, `file://${process.cwd()}/`).href;
+  let staticServer;
+  if (options.localhost) {
+    staticServer = new StaticServer(options.port, source);
+    await staticServer.start();
+  }
+  const src = options.localhost
+    ? staticServer.url.href
+    : new URL(source, `file://${process.cwd()}/`).href;
+  log.info(`Processing resource: ${src} ...`, options.timeout * 1000);
 
   const { html, errors, warnings } = await toHTML(src, {
     timeout: options.timeout * 1000,
+    useLocal: options["use-local"],
     onError: log.error.bind(log),
     onWarning: log.warn.bind(log),
     onProgress: log.info.bind(log),
@@ -119,4 +242,6 @@ async function run(source, destination, options, log) {
   }
 
   await write(destination, html);
+
+  if (staticServer) await staticServer.stop();
 }
